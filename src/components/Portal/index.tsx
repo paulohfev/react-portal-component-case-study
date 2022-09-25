68e93baf5589fbaa552cc166b12de78e3cf6aafa
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-09-25 14:35:18 -0300

    Add case for dynamically creating root element

--- src/components/Portal/index.tsx
+++ src/components/Portal/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 type Props = {
@@ -6,8 +6,27 @@ type Props = {
   rootSelector: string;
 }
 
-const Portal: React.FC<Props> = ({ children, rootSelector }) => {
-  return createPortal(children, document.getElementById(rootSelector) as HTMLElement);
-}
+const Portal: React.FC<Props> = ({ rootSelector, children }) => {
+  const element = useRef(document.getElementById(rootSelector));
+
+  useEffect(() => {
+    return () => {
+      if (!!element.current) {
+        if (element.current.childNodes.length === 0) {
+          element.current.remove();
+          element.current = null;
+        }
+      }
+    };
+  }, [rootSelector, element]);
+
+  if (!element.current) {
+    element.current = document.createElement("div");
+    element.current.setAttribute("id", rootSelector);
+    document.body.appendChild(element.current);
+  }
+
+  return createPortal(children, element.current);
+};
 
 export default Portal;
